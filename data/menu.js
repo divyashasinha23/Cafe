b830999a952621e09ae8ae72dfff4498d516f1ce
Anisha <agarwalanisha1111@gmail.com>
2020-12-29 16:59:42 +0530

    menu added

---
+++ data/menu.js
@@ -0,0 +1,37 @@
+const menu=[
+   {
+       _id:1,
+       dish_name:'Cookies',
+       dish_price:15
+   },
+   {
+       _id:2,
+       dish_name:'veg Samosa',
+       dish_price:25
+   },
+   {
+       _id:3,
+       dish_name:'Garlic Bread',
+       dish_price:50
+    
+    },
+    {
+        _id:4,
+        dish_name:'Classic Cappuccino',
+        dish_price:100
+    },
+
+
+]
+
+module.exports=menu;
+
+
+
+
+
+
+
+
+
+]
